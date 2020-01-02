c8847ad056353ba57251532fc40962d6d29e06aa
Adeeb Mohammed <adeeb2358@gmail.com>
2020-01-02 23:11:27 +0900

    solution using dynamic programming as well as pure recursion is also included

--- FebNumber/main.cpp
+++ FebNumber/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include <unordered_map>
 
 /*
     program to find the nth febonocci number
@@ -35,13 +36,59 @@ int getNthFib(int n) {
     return sumOfBoth;
 }
 
+// febenocci numbers using recursion
+// the time o(n2) and space complexity o(n)
+int getNthFibRecur(int n){
+    int sumOfBoth = 0;
+    if(1 == n || n < 1){
+        return 0;
+    }
+    if(2 == n){
+        return 1;
+    }
+    sumOfBoth  = getNthFibRecur(n-1) + getNthFibRecur(n-2);
+    return sumOfBoth;
+}
+
+// febenocci numbers using dynamic programming
+// it simply means repeated steps can be stored in some
+// hash table and access this in constant time
+
+int findFib(int n, std::unordered_map<int, int> memoize){
+    if(memoize.find(n) != memoize.end()){
+        return memoize[n];
+    }else{
+        memoize[n] = findFib(n-1,memoize) + findFib(n-2,memoize);
+        return memoize[n];
+    }
+}
+
+int getNthFibDyn(int n){
+    int resultNumber = 0;
+    std::unordered_map<int, int> memoize;
+    memoize[1] = 0;
+    memoize[2] = 1;
+    resultNumber = findFib(n,memoize);
+    memoize.clear();
+    return resultNumber;
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    // optismal solution output
     std::cout << "getNthFib(1)= " << getNthFib(1)<< std::endl;
     std::cout << "getNthFib(2)= " << getNthFib(2)<< std::endl;
     std::cout << "getNthFib(3)= " << getNthFib(3)<< std::endl;
     std::cout << "getNthFib(4)= " << getNthFib(4)<< std::endl;
     std::cout << "getNthFib(5)= " << getNthFib(5)<< std::endl;
     std::cout << "getNthFib(6)= " << getNthFib(6)<< std::endl;
+    // recursion very slow soluton output
+    std::cout << "getNthFibRecur(1)= " << getNthFibRecur(1)<< std::endl;
+    std::cout << "getNthFibRecur(2)= " << getNthFibRecur(2)<< std::endl;
+    std::cout << "getNthFibRecur(3)= " << getNthFibRecur(3)<< std::endl;
+    std::cout << "getNthFibRecur(4)= " << getNthFibRecur(4)<< std::endl;
+    std::cout << "getNthFibRecur(5)= " << getNthFibRecur(5)<< std::endl;
+    std::cout << "getNthFibRecur(6)= " << getNthFibRecur(6)<< std::endl;
+    
+    
     return 0;
 }
