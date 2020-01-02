fe6a7616a31b1741ef1f8a54b245364eac909cc3
Adeeb Mohammed <adeeb2358@gmail.com>
2020-01-02 22:00:21 +0900

    nth febenocci number space o(1) and time o(n)

--- FebNumber/main.cpp
+++ FebNumber/main.cpp
@@ -8,8 +8,40 @@
 
 #include <iostream>
 
+/*
+    program to find the nth febonocci number
+    sample input 6
+    output 5 [0 1 1 2 3 5]
+ */
+
+int getNthFib(int n) {
+    
+    if(1 == n ){
+        return 0;
+    }
+    
+    if(2 == n || 3 == n ){
+        return 1;
+    }
+    
+    int prevNumOne = 1;
+    int prevNumTwo = 1;
+    int sumOfBoth  = 0;
+    for(int i = 3; i < n; i++){
+        sumOfBoth  = prevNumTwo + prevNumOne;
+        prevNumOne = prevNumTwo;
+        prevNumTwo = sumOfBoth;
+    }
+    return sumOfBoth;
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "getNthFib(1)= " << getNthFib(1)<< std::endl;
+    std::cout << "getNthFib(2)= " << getNthFib(2)<< std::endl;
+    std::cout << "getNthFib(3)= " << getNthFib(3)<< std::endl;
+    std::cout << "getNthFib(4)= " << getNthFib(4)<< std::endl;
+    std::cout << "getNthFib(5)= " << getNthFib(5)<< std::endl;
+    std::cout << "getNthFib(6)= " << getNthFib(6)<< std::endl;
     return 0;
 }
